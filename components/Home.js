584875de0d73e51fb280621fc4186a21016f7141
Wendy Gilles <wendygilles@hotmail.com>
2023-04-27 13:18:49 +0200

    jte passe le flambeau Camille

--- components/Home.js
+++ components/Home.js
@@ -1,12 +1,29 @@
-import styles from '../styles/Home.module.css';
+import styles from "../styles/Home.module.css";
+import Image from 'next/image'
 
 function Home() {
   return (
     <div>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <div className={styles.leftContainer}>
+          <div className={styles.mascotte}>
+            <Image src='/mascotte.png' width={600} height={500} alt="photo" />
+          </div>
+        </div>
+        <div className={styles.rightContainer}>
+          <div className={styles.logo}>
+            <img alt="Logo" />
+          </div>
+          <div className={styles.title}>
+            <h1>See what's happening</h1>
+            <h2>Join hackatweet today.</h2>
+          </div>
+          <div className={styles.button}>
+            <button className={styles.signupBtn}>Sign up</button>
+            <p>Already have an account ?</p>
+            <button className={styles.signinBtn}>Sign in</button>
+          </div>
+        </div>
       </main>
     </div>
   );
